950dff4ce0bc8989870ff46539faa8cd6a8751a6
Diogo Reis <Diogo.Reis.T0111275@edu.atec.pt>
2020-06-22 16:45:42 +0100

    start of RestaurantInfo

---
+++ Data/RestaurantInfo/Booking.cs
@@ -0,0 +1,34 @@
+﻿using Data.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.RestaurantInfo
+{
+    public class Booking : Entity
+    {
+        private DateTime _date;
+
+        [Required]
+        [Display(Name = "Date")]
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                RegisterChange();
+            }
+        }
+
+        public Booking(DateTime date)
+        {
+            _date = date;
+
+        }
+        public Booking(Guid id, DateTime createdAt, DateTime updatedAt, bool isDeleted, DateTime date) : base(id, createdAt, updatedAt, isDeleted)
+        {
+            _date = date;
+
+        }
+    }
+}
---
+++ Data/RestaurantInfo/StaffTitle.cs
@@ -0,0 +1,30 @@
+﻿using Data.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.RestaurantInfo
+{
+    public class StaffTitle : Entity
+    {
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate;
+
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime EndDate;
+
+        public StaffTitle(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+
+        }
+        public StaffTitle(Guid id, DateTime createdAt, DateTime updatedAt, bool isDeleted, DateTime startDate, DateTime endDate) : base(id, createdAt, updatedAt, isDeleted)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+
+        }
+    }
+}
