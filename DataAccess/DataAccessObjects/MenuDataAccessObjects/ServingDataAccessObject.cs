e4085274c7df7a8e5582510e318a91927bc7f36a
Edson Correia <bioeugenio10@hotmail.com>
2020-06-24 19:40:21 +0100

    DataAccessObjects

---
+++ DataAccess/DataAccessObjects/MenuDataAccessObjects/ServingDataAccessObject.cs
@@ -0,0 +1,105 @@
+﻿using Data.MenuInfo;
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataAccessObjects.MenuDataAccessObjects
+{
+    public class ServingDataAccessObject
+    {
+        private RestaurantContext _context;
+
+        public ServingDataAccessObject()
+        {
+            _context = new RestaurantContext();
+        }
+
+        #region C
+
+        public void Create(Serving serving)
+        {
+            _context.Servings.Add(serving);
+            _context.SaveChanges();
+        }
+
+        public async Task CreateAsync(Serving serving)
+        {
+            await _context.Servings.AddAsync(serving);
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+
+        #region R
+
+        public Serving Read(Guid id)
+        {
+            return _context.Servings.FirstOrDefault(x => x.Id == id);
+
+        }
+
+        public async Task<Serving> ReadAsync(Guid id)
+        {
+            return await
+                new Task<Serving>(() => _context.Servings.FirstOrDefault(x => x.Id == id));
+
+        }
+        #endregion
+
+        #region U
+
+        public void Update(Serving serving)
+        {
+            _context.Entry(serving).State = EntityState.Modified;
+            _context.SaveChanges();
+
+        }
+
+        public async Task UpdateAsync(Serving serving)
+        {
+            _context.Entry(serving).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+        }
+
+        #endregion
+
+        #region D
+
+        public void Delete(Serving serving)
+        {
+            serving.IsDeleted = true;
+            Update(serving);
+
+        }
+
+        public void Delete(Guid id)
+        {
+            var item = Read(id);
+            if (item == null) return;
+            Delete(item);
+
+        }
+
+        public async Task DeleteAsync(Serving serving)
+        {
+            serving.IsDeleted = true;
+            await UpdateAsync(serving);
+
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var item = ReadAsync(id).Result;
+            if (item == null) return;
+            await DeleteAsync(item);
+
+        }
+        #endregion
+
+    }
+}
---
+++ DataAccess/DataAccessObjects/RestaurantDataAccessObjects/BookingDataAccessObject.cs
@@ -0,0 +1,104 @@
+﻿using Data.RestaurantInfo;
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataAccessObjects.RestaurantDataAccessObjects
+{
+    public class BookingDataAccessObject
+    {
+        private RestaurantContext _context;
+
+        public BookingDataAccessObject()
+        {
+            _context = new RestaurantContext();
+        }
+
+        #region C
+
+        public void Create(Booking booking)
+        {
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+        }
+
+        public async Task CreateAsync(Booking booking)
+        {
+            await _context.Bookings.AddAsync(booking);
+            await _context.SaveChangesAsync();
+        }
+        #endregion
+
+
+        #region R
+
+        public Booking Read(Guid id)
+        {
+            return _context.Bookings.FirstOrDefault(x => x.Id == id);
+
+        }
+
+        public async Task<Booking> ReadAsync(Guid id)
+        {
+            return await
+                new Task<Booking>(() => _context.Bookings.FirstOrDefault(x => x.Id == id));
+
+        }
+        #endregion
+
+        #region U
+
+        public void Update(Booking booking)
+        {
+            _context.Entry(booking).State = EntityState.Modified;
+            _context.SaveChanges();
+
+        }
+
+        public async Task UpdateAsync(Booking booking)
+        {
+            _context.Entry(booking).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+        }
+
+        #endregion
+
+        #region D
+
+        public void Delete(Booking booking)
+        {
+            booking.IsDeleted = true;
+            Update(booking);
+
+        }
+
+        public void Delete(Guid id)
+        {
+            var item = Read(id);
+            if (item == null) return;
+            Delete(item);
+
+        }
+
+        public async Task DeleteAsync(Booking booking)
+        {
+            booking.IsDeleted = true;
+            await UpdateAsync(booking);
+
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var item = ReadAsync(id).Result;
+            if (item == null) return;
+            await DeleteAsync(item);
+
+        }
+        #endregion
+    }
+}
