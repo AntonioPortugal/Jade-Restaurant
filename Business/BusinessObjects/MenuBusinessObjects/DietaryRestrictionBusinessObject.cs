dca06a110c5dd59e55f797047fc3ac8b1d314906
António Portugal <vasc.antonio@gmail.com>
2020-06-25 18:24:32 +0100

    added two more business objects

---
+++ Business/BusinessObjects/MenuBusinessObjects/DietaryRestrictionBusinessObject.cs
@@ -0,0 +1,275 @@
+﻿using RECODME.RD.Jade.Business.OperationResults;
+using RECODME.RD.Jade.Data.MenuInfo;
+using RECODME.RD.Jade.DataAccess.DataAccessObjects.MenuDataAccessObjects;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RECODME.RD.Jade.Business.BusinessObjects.MenuBusinessObjects
+{
+    public class DietaryRestrictionBusinessObject
+    {
+        private DietaryRestrictionDataAccessObject _dao;
+
+        public DietaryRestrictionBusinessObject()
+        {
+            _dao = new DietaryRestrictionDataAccessObject();
+
+        }
+
+        #region C
+
+        public OperationResult Create(DietaryRestriction item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Create(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> CreateAsync(DietaryRestriction item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.CreateAsync(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region R
+
+        public OperationResult<DietaryRestriction> Read(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var result = _dao.Read(id);
+                transactionScope.Complete();
+                return new OperationResult<DietaryRestriction>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<DietaryRestriction>() { Success = false, Exception = e };
+
+            }
+
+
+        }
+
+        public async Task<OperationResult<DietaryRestriction>> ReadAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.ReadAsync(id);
+                transactionScope.Complete();
+                return new OperationResult<DietaryRestriction>() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<DietaryRestriction>() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region U
+
+        public OperationResult Update(DietaryRestriction item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Update(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> UpdateAsync(DietaryRestriction item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.UpdateAsync(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region D
+
+        public OperationResult Delete(DietaryRestriction item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Delete(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.DeleteAsync(id);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public OperationResult Delete(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Delete(id);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> DeleteAsync(DietaryRestriction item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.DeleteAsync(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Business/BusinessObjects/MenuBusinessObjects/DishBusinessObject.cs
@@ -0,0 +1,274 @@
+﻿using RECODME.RD.Jade.Business.OperationResults;
+using RECODME.RD.Jade.Data.MenuInfo;
+using RECODME.RD.Jade.DataAccess.DataAccessObjects.MenuDataAccessObjects;
+using System;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RECODME.RD.Jade.Business.BusinessObjects.MenuBusinessObjects
+{
+    public class DishBusinessObject
+    {
+        private DishDataAccessObject _dao;
+
+        public DishBusinessObject()
+        {
+            _dao = new DishDataAccessObject();
+
+        }
+
+        #region C
+
+        public OperationResult Create(Dish item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Create(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> CreateAsync(Dish item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.CreateAsync(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region R
+
+        public OperationResult<Dish> Read(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                var result = _dao.Read(id);
+                transactionScope.Complete();
+                return new OperationResult<Dish>() { Success = true, Result = result };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<Dish>() { Success = false, Exception = e };
+
+            }
+
+
+        }
+
+        public async Task<OperationResult<Dish>> ReadAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.ReadAsync(id);
+                transactionScope.Complete();
+                return new OperationResult<Dish>() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult<Dish>() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region U
+
+        public OperationResult Update(Dish item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Update(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> UpdateAsync(Dish item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.UpdateAsync(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+        #region D
+
+        public OperationResult Delete(Dish item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Delete(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        public async Task<OperationResult> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.DeleteAsync(id);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public OperationResult Delete(Guid id)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                _dao.Delete(id);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+        public async Task<OperationResult> DeleteAsync(Dish item)
+        {
+            try
+            {
+                var transactionOptions = new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TimeSpan.FromSeconds(30)
+
+                };
+                var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+                await _dao.DeleteAsync(item);
+                transactionScope.Complete();
+                return new OperationResult() { Success = true };
+
+            }
+            catch (Exception e)
+            {
+                return new OperationResult() { Success = false, Exception = e };
+
+            }
+
+        }
+
+        #endregion
+
+    }
+}
