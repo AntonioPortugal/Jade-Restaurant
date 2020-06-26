246958bbbf473d4967a74e27385c4d0e03d6dc59
Edson Correia <bioeugenio10@gmail.com>
2020-06-26 01:06:53 +0100

    Serving

---
+++ Business/BusinessObjects/MenuBusinessObjects/ServingBusinessObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.BusinessObjects.MenuBusinessObjects
+{
+    public class ServingBusinessObject
+    {
+
+    }
+}
